d1b0e36c9d0588828de60f2d22434d2959e7b6cb
AlexKravchenko0412 <alexkravchenko0412@gmail.com>
2021-11-18 00:05:37 +0300

    18.11.21 RecycleView done. Gotta check layout

--- app/src/main/java/android/example/autodata/car/AutoActivity.java
+++ app/src/main/java/android/example/autodata/car/AutoActivity.java
@@ -24,84 +24,21 @@ import java.util.List;
 
 public class AutoActivity extends AppCompatActivity {
 
-    private final String DATA_URL = "https://api.openweathermap.org/data/2.5/weather?q=Санкт-Петербург&appid=b47d1fe9f6c8abcc2c5ced5c59413e6d";
+
 
    // ArrayList<CarDataPackage> dataPackage = new ArrayList<>();
-   List<CarDataPackage> items = new ArrayList<>();
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auto);
-
-        DownloadData downloadData = new DownloadData();
-        downloadData.execute(DATA_URL);
         RecyclerView recyclerView = findViewById(R.id.recViewCar);
-        CarAdapter carAdapter = new CarAdapter(this,items);
+
+
+        CarAdapter carAdapter = new CarAdapter(this);
         recyclerView.setAdapter(carAdapter);
-
-    }
-
-
-    private class DownloadData extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected String doInBackground(String... strings) {
-            URL url = null;
-            URLConnection urlConnection = null;
-            StringBuilder result = new StringBuilder();
-            try {
-                url = new URL(strings[0]);
-                urlConnection = (HttpURLConnection) url.openConnection();
-                InputStream inputStream = urlConnection.getInputStream();
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                String line = bufferedReader.readLine();
-                while (line != null) {
-                    result.append(line);
-                    line = bufferedReader.readLine();
-                }
-                return result.toString();
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (urlConnection != null) {
-                    ((HttpURLConnection) urlConnection).disconnect();
-                }
-
-            }
-
-            return null;
-        }
-
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            try {
-
-                if (s != null) {
-                    JSONObject jsonObject = new JSONObject(s);
-                    String city = jsonObject.getString("name");
-                    String temp = jsonObject.getJSONObject("main").getString("temp");
-                    String description = jsonObject.getJSONArray("weather").getJSONObject(0).
-                            getString("description");
-                    items.add(new CarDataPackage(city, city, R.drawable.warning_img));
-                    items.add(new CarDataPackage(temp, temp, R.drawable.warning_img));
-                    items.add(new CarDataPackage(description, description, R.drawable.warning_img));
-                    //ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),
-                     //       android.R.layout.simple_list_item_1, items);
-                    //listViewTest.setAdapter(adapter);
-
-                    //String weather = String.format("%s\nTemp: %s\n%s", city, temp, description);
-                    //textViewTest.setText(weather);
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-        }
 
     }
 
