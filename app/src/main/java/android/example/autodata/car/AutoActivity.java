e59fb46b981b3eeb7ae765f78b284e4553fe1079
AlexKravchenko0412 <alexkravchenko0412@gmail.com>
2021-11-22 15:49:24 +0300

    BottomNavigation works properly

--- app/src/main/java/android/example/autodata/car/AutoActivity.java
+++ app/src/main/java/android/example/autodata/car/AutoActivity.java
@@ -1,18 +1,32 @@
 package android.example.autodata.car;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
-import android.example.autodata.R;
-import android.os.Bundle;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.example.autodata.R;
+import android.example.autodata.settings.SettingsActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class AutoActivity extends AppCompatActivity {
 
 
     // ArrayList<CarDataPackage> dataPackage = new ArrayList<>();
+    private ImageView auto;
+    private ImageView stock;
+    private ImageView offer;
+    private ImageView settings;
 
 
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +35,37 @@ public class AutoActivity extends AppCompatActivity {
         CarAdapter carAdapter = new CarAdapter(this);
         recyclerView.setAdapter(carAdapter);
 
+        auto = findViewById(R.drawable.auto_img);
+        stock = findViewById(R.drawable.stock_img);
+        offer = findViewById(R.drawable.offers_img);
+        settings = findViewById(R.drawable.settings_img);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavViewMenu);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_auto:
+                        //auto.setVisibility(View.VISIBLE);
+                        //stock.setVisibility(View.GONE);
+                        //offer.setVisibility(View.GONE);
+                        //settings.setVisibility(View.GONE);
+                        break;
+                    case R.id.settings:
+                        //auto.setVisibility(View.GONE);
+                        //stock.setVisibility(View.GONE);
+                        //offer.setVisibility(View.GONE);
+                        //settings.setVisibility(View.VISIBLE);
+                        Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+                        startActivity(intent);
+                        break;
+                }
+                return false;
+            }
+        });
+
     }
+
 
 }
