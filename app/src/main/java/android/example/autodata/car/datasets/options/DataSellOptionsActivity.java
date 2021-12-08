fd345e26cf4b1c1378bedae56bcd0fc341fa76ca
AlexKravchenko0412 <alexkravchenko0412@gmail.com>
2021-12-08 17:27:31 +0300

    Data Sell Options

---
+++ app/src/main/java/android/example/autodata/car/datasets/options/DataSellOptionsActivity.java
@@ -0,0 +1,15 @@
+package android.example.autodata.car.datasets.options;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.example.autodata.R;
+import android.os.Bundle;
+
+public class DataSellOptionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_sell_options);
+    }
+}
