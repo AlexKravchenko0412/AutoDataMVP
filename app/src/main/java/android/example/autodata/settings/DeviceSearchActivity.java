11a4b5aceac1de27fba44c873754819f78890684
AlexKravchenko0412 <alexkravchenko0412@gmail.com>
2021-12-24 16:25:30 +0300

    Paired devices Bluetooth

--- app/src/main/java/android/example/autodata/settings/DeviceSearchActivity.java
+++ app/src/main/java/android/example/autodata/settings/DeviceSearchActivity.java
@@ -17,13 +17,17 @@ import java.util.Set;
 
 public class DeviceSearchActivity extends AppCompatActivity {
 
-    private static final int REQUEST_ENABLE_BT = 0;
-    private static final int REQUEST_DISCOVER_BT = 1;
+    //private static final int REQUEST_ENABLE_BT = 0;
+    //private static final int REQUEST_DISCOVER_BT = 1;
+    private int REQUEST_CODE = 1;
+    private Set<BluetoothDevice> pairedDevices;
+    private ArrayList<String> listPairedDevices;
+
 
     private Button btnDeviceSearch;
 
     BluetoothAdapter mBlueAdapter;
-    private ArrayList<String> pairedDevs;
+    //private ArrayList<String> pairedDevs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,61 +35,44 @@ public class DeviceSearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_device_search);
         btnDeviceSearch = findViewById(R.id.btnDeviceSearch);
 
-        mBlueAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (mBlueAdapter == null) {
-            showToast("Bluetooth недоустпен");
-        }
-
         btnDeviceSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              //  if (!mBlueAdapter.isEnabled()) {
-             //       showToast("Bluetooth включается");
-              //      Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-              //      startActivityForResult(intent, REQUEST_ENABLE_BT);
-              //  } else {
-              //      showToast("Bluetooth уже включен");
-              //  }
-
-                if(!mBlueAdapter.isDiscovering()) {
-                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-                    startActivityForResult(intent,REQUEST_DISCOVER_BT);
-                }
-
-               // if(mBlueAdapter.isEnabled()) {
-             //       mBlueAdapter.disable();
-               //     showToast("Bluetooth выключается");
-              //  }
-              //  else {
-              //      showToast("Bluetooth уже выключен");
-              //  }
-
-                if(mBlueAdapter.isEnabled()) {
-                    Set<BluetoothDevice> devices = mBlueAdapter.getBondedDevices();
-                    for(BluetoothDevice device : devices) {
-                        pairedDevs.add(device.getName());
+                mBlueAdapter = BluetoothAdapter.getDefaultAdapter();
+                if (mBlueAdapter == null) {
+                    showToast("Bluetooth недоустпен");
+                } else {
+                    if (!mBlueAdapter.isEnabled()) {
+                        Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                        startActivityForResult(intent, REQUEST_CODE);
                     }
-                }
-                else {
-                    showToast("Bluetooth выключен");
                 }
             }
         });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        switch (requestCode) {
-            case REQUEST_ENABLE_BT:
-                if (resultCode == RESULT_OK) {
-                    showToast("Bluetooth включен");
-                } else {
-                    showToast("Не удалось включить Bluetooth");
-                }
-                break;
-        }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
         super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE)
+        {
+           if(resultCode==RESULT_OK)
+           {
+               showToast("Bluetooth on");
+               pairedDevices = mBlueAdapter.getBondedDevices();
+               int count = pairedDevices.size();
+               for(BluetoothDevice device : pairedDevices)
+               {
+                   listPairedDevices.add(device.getName().toString());
+                   count++;
+               }
+           }
+
+
+        }
     }
+
 
     private void showToast(String msg) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
